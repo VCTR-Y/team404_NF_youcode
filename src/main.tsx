c17e31df5ed5ac285b03480febc5ca7b5e9db8f3
pmruthyu <pranav.karthik10@gmail.com>
2025-04-05 14:20:01 -0700

    add chakra

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import { Provider } from "@/components/ui/provider"
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </StrictMode>,
 )

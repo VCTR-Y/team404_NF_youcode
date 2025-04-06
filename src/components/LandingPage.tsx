fe87d189bfe4d092ef722b9f92412c73dbe0daa5
pmruthyu <pranav.karthik10@gmail.com>
2025-04-06 10:50:43 -0700

    update landing page

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,22 +1,26 @@
 import { Link } from "react-router-dom"; // lets you create internal links without reloading the page (click the button and go to a new page)
 import { Button } from "@/components/ui/button"; // (standard UI library) designs the button to standardize all button styles across your app
 import foodSharing from "/images/IMG_4516.jpg"; // Path to the image
+import { ModeToggle } from "./mode-toggle";
 
 export function PlatefulLandingPage() {
   // a React function component
   return (
     // output (JSX) of your component begins
 
-    <div className="min-h-screen flex flex-col justify-between bg-white">
-      <div className="flex justify-between items-center w-full p-6 bg-white">
-        <div className="text-4xl text-black font-bold">Plateful</div>
-        <Button>
+    <div className="h-full w-full flex flex-col justify-between">
+      <div className="flex justify-between items-center w-full">
+        <div className="text-4xl font-bold">Plateful</div>
+        <div className="flex items-center gap-4">
+            <ModeToggle />
+            <Button>
           <Link to="/login">Login</Link>
-        </Button>
+            </Button>
+        </div>
       </div>
 
       <div className="flex flex-col justify-center items-center text-center flex-grow px-4">
-        <h1 className="text-5xl font-bold mt-12 mb-6 tracking-tight text-gray-800">
+        <h1 className="text-5xl font-bold mt-12 mb-6 tracking-tight">
           Donate or Receive Food with Plateful
         </h1>
         <p className="text-lg text-muted-foreground max-w-2xl leading-relaxed">
